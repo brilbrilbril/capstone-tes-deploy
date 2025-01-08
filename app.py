7fa0949be71dbbc1596e72c80f3596e884b09ab0
brilbrilbril <maghatan@gmail.com>
2025-01-08 13:06:28 +0700

    change to vdb based

--- app.py
+++ app.py
@@ -11,8 +11,14 @@ from crewai.process import Process
 # retrieve purchase history tool 
 from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
+import openai
+from langchain.vectorstores import FAISS
+# from langchain.embeddings import OpenAIEmbeddings
+from langchain.schema import Document
+import os
+import tiktoken
 import re
 from gradio_client import Client as GradioClient, file, handle_file
 import cv2
@@ -25,181 +31,48 @@ os.environ["OPENAI_API_KEY"] = openai_api_key
 
 #csv_search_tool_history = CSVSearchTool("Dataset/Customer_Interaction_Data.csv")
 
-csv_search_tool_product = CSVSearchTool("Dataset/final_product_catalog.csv")
+#csv_search_tool_product = CSVSearchTool("Dataset/final_product_catalog.csv")
 df = pd.read_csv('Dataset/Customer_Interaction_Data_v2.csv')
 df_products = pd.read_csv('Dataset/final_product_catalog.csv')
 
-llm = ChatOpenAI(openai_api_key=openai_api_key,model_name='gpt-3.5-turbo', temperature=0.1)
-llm_product_recommender = ChatOpenAI(openai_api_key=openai_api_key,model_name='gpt-4o-mini', temperature=0.1)
+# 1. Create Vector Database
+def load_vector_db():
+    embeddings = OpenAIEmbeddings(model="text-embedding-ada-002", openai_api_key=openai.api_key)
+    #vector_db = FAISS.from_documents(documents, embeddings)
+    vector_db = FAISS.load_local('faiss_index', embeddings, allow_dangerous_deserialization=True)
+    return vector_db
 
-system_prompt = (
-    "Use the given context to answer the question. "
-    "If you don't know the answer, say you don't know. "
-    "Accept context that shows about data, besides that say that the context is prohibited. "
-    "Give detailed answer, but don't make it a table. "
-    "Use this following format: "
-    "   Purchased history: "
-        "- Customer ID: "
-        "- Product ID: "
-        "- Transaction ID: "
-        "- Purchase Date: "
-        "- Order Value: "
-        "- Product Details: "
-    "Context: {context}"
-)
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{context}"),
-    ]
-)
+def retrieve_transcation(cust_id):
+    return df[df['Customer_ID'] == cust_id].head(3).to_dict()
 
-csv_agent = create_csv_agent(
-    ChatOpenAI(temperature=0, model="gpt-3.5-turbo"),
-    "Dataset/Customer_Interaction_Data_v2.csv",
-    verbose=True,
-    agent_type=AgentType.OPENAI_FUNCTIONS,
-    allow_dangerous_code=True,
-    prompt=prompt
-)
+# 2. Retrieval Agent
+def retrieve_documents(query, vector_db, top_k=5):
+    return vector_db.similarity_search(query, top_k)
 
-class PurchaseHistoryInfo(BaseTool):
-    name: str = "Purchase History Information Tool"
-    description: str = "Search past history based on specific task and customer"
+# 3. Generator Agent
+def generate_response_openai(query, docs, purchase_hist):
+    # Combine retrieved documents into context
+    context = "\n\n".join([doc.page_content for doc in docs])
+    prompt = (
+        f"Answer the following question based on the context:\n\nContext: {context}\n by providing a similarity with user purchase history:\n\n History : {purchase_hist}\n\n Question: {query}. "
+        "Provide detailed and accurate answer with maximum 3 products. "
+        "Always include the reason. "
+        "If the question is product related, always attach product id"
+    )
 
-    def _run(self, query: str) -> str:
-        return csv_agent.run(query)
+    completion = openai.chat.completions.create(
+        model="gpt-4o",  # Adjust the model name as per availability
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant. Answer accurately and give reason"},
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return completion.choices[0].message.content
 
-# agent to retrieve purchase history
-purchase_history_retriever_agent = Agent(
-    role='Purchase history retriever',
-    goal='Find the purchase history specifically for the {customer}.',
-    backstory=(
-        "You are a meticulous Purchase History Retrieval working at Fashion company "
-        "ensuring accurate and up-to-date data gotten from the database. "
-        "is available for solving customers queries."
-        "You need to make sure that the data is relevant and accurate."
-        "Prodive the Customer_ID, Product_ID, Transaction_ID, Purchase_Date, Order_Value, and Product_Details."
-        ),
-    llm=llm,
-    verbose=True,
-    allow_delegation=True,
-    tools=[PurchaseHistoryInfo()]
-)
-
-#agent to retrieve products
-product_catalog_retriever_agent = Agent(
-    role='Product Catalog Retriever',
-    goal='Find the related product based on customer task: {query}',
-    backstory=(
-        "You are working on a task: {query} to provide the customer. "
-        "Retrieve the specific output from the specific task: {query}. Provide the list of products, including Product_ID, Rating, Category, Size, Color, Price, Weather, and Event Type."
-        "You need to make sure that the data is relevant and accurate. "
-        "Provide with at least 5 products."
-    ),
-    llm=llm,
-    verbose=True,
-    allow_delegation=True,
-    tools=[csv_search_tool_product]
-)
-
-review_agent = Agent(
-    role='Result Reviewer',
-    goal="Review the response drafted by the Purchase history retriever and Product Catalog Retriever for {customer}'s inquiry: {query}. ",
-    backstory=(
-        "You are working on a task: {query} to provide the customer. "
-        "Review the response drafted by the Purchase history retriever and Product Catalog Retriever for {customer}'s inquiry: {query}. "
-    ),
-    llm=llm,
-    verbose=True
-)
-
-product_recommender_agent = Agent(
-    role='Product Recommendation Provider',
-    goal="Give product recommendation based on review from Result Reviewer. ",
-    backstory=(
-        "You are working on a task: {query} to give product recommendation. "
-        "Analyze the product details from customer purchase history and product catalog. "
-        "Find the correlation between them to provide best product recommendation. "
-    ),
-    llm=llm_product_recommender,
-    verbose=True
-)
-
-# purchase history task
-retrieve_purchase_history_task = Task(
-    description=(
-        "Gather all relevant {customer} data from the dataset, focusing "
-        "on crucial data which will be great to know when addressing the "
-        "customer's inquiry."
-        "Give accurate answer and no assumption. Return '{customer} is not available' if the customer is not in the dataset."
-        ),
-    expected_output=("A list of the customer's information. "
-      "Turn them into bullets. "
-      "Highlighting key info of the customer that will be helpful "
-      "to the team when addressing the customer's query. "
-    ),
-    agent=purchase_history_retriever_agent
-)
-
-#product catalog task
-retrieve_product_catalog_task = Task(
-    description=("customer is asking for specific task: {query}. "
-      "Analyze the Product_Catalog_Data.csv and make sure the task from customer is answered accurately. "
-      "Make sure that you provide the best and relevant answer. "
-      "Give accurate answer and no assumption."
-      "Use 'search_query: {query}' for tool input."
-    ),
-    expected_output=("List of 5 product(s) with product ID that match user task."
-    ),
-    agent=product_catalog_retriever_agent,
-)
-
-#Review Result Task
-review_result_task = Task(
-    description=(
-        "Review the purchase history from Purchase History Retriever and the products"
-        "from Product Catalog Retriever."
-    ),
-    expected_output=(
-        "List all of received purchase history from Purchase History Retriever with following format: "
-        "1. Purchased history: "
-        "- Customer ID: "
-        "- Product ID: "
-        "- Transaction ID: "
-        "- Purchase Date: "
-        "- Order Value: "
-        "and list of prodcut from Product Catalog Retriever with following format: "
-        "2. Product Catalog: "
-    ),
-    agent=review_agent,
-    context=[retrieve_purchase_history_task, retrieve_product_catalog_task]
-)
-
-#Product Recommendation TAsk
-product_recommendation_task = Task(
-    description=(
-        "Analyze the product details from user purchase history and product catalog received only from Result Reviewer "
-        "to give best product recommendation to user."
-        "You are not allowed to search any product besides from Result Reviewer."
-    ),
-    expected_output=(
-        "List of maximum 3 product recommendation after find the similarity or relevance between  product details from user purchase history and product catalog from Result Reviewer. "
-        "Give the reason to convince customer. "
-        "Do not make any assumption, just based on received user purchase history and product catalog. "
-        "If the product is not available, return that the product is not in product catalog."
-    ),
-    agent=product_recommender_agent,
-    context=[review_result_task]
-)
-
-crew = Crew(
-    agents=[purchase_history_retriever_agent, product_catalog_retriever_agent, review_agent, product_recommender_agent],
-    tasks=[retrieve_purchase_history_task, retrieve_product_catalog_task, review_result_task, product_recommendation_task],
-    verbose=True,
-    memory=False,
-    process=Process.sequential,
-)
+# 4. Multi-Agent System
+def multi_agent_rag(query, vector_db, purchase_hist):
+    retrieved_docs = retrieve_documents(query, vector_db)
+    return generate_response_openai(query, retrieved_docs, purchase_hist)
 
 # Streamlit Interface
 st.title("💬 Product Recommendation Chatbot")
@@ -274,8 +147,9 @@ if prompt := st.chat_input(placeholder="Type here for recommend product..."):
         # Proses permintaan dengan Crew
         try:
             inputs = {"query": prompt, "customer": st.session_state["customer_id"]}
-            response = crew.kickoff(inputs=inputs)
-            response = response.raw
+            vector_db = load_vector_db()
+            transaction_data = retrieve_transcation(st.session_state["customer_id"])
+            response = multi_agent_rag(inputs['query'], vector_db, transaction_data)
         except Exception as e:
             response = f"An error occurred: {e}"
 
@@ -287,8 +161,9 @@ if prompt := st.chat_input(placeholder="Type here for recommend product..."):
     output = response
     print(output)
     # Regular expression pattern to extract Product ID
-    pattern = r"Product[_ ]ID: (\w+)"
+    pattern = r"(?i)\b(PROD\d+)\b"
     product_ids = re.findall(pattern, output)
+    print(product_ids)
     # Menghilangkan duplikasi
     unique_product_ids = list(set(product_ids))
     st.session_state.product_ids = unique_product_ids
@@ -324,7 +199,9 @@ if st.session_state.product_ids:
         if uploaded_image:
             st.session_state.uploaded_image = uploaded_image  # Store uploaded image in session state
             st.session_state.uploaded_image_name = uploaded_image.name  # Store image name
-
+            image = Image.open(uploaded_image)
+            image.thumbnail((300,600))
+            st.image(image, caption="Your image")
             with open(uploaded_image.name, "wb") as f:
                 f.write(uploaded_image.getbuffer())
             st.success(f"Image uploaded and saved successfully!")
